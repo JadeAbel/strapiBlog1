213111dce92800541ef6260d33a6bd1ae7397efa
Jade Abel <jade.marie.abel@gmail.com>
2022-07-26 19:52:34 -0400

    updates to categories

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1a025be09e021f5db0a5"; }
+/******/ 		__webpack_require__.h = function() { return "0ce6d0508389aa07b025"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
